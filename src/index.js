1d203a92baec940a4fcd015983af61e15d33901b
Eric Martins <eric.martins@radixeng.com.br>
2021-03-11 18:45:10 -0300

    ensures update method cannot update the numbers of likes

--- src/index.js
+++ src/index.js
@@ -32,15 +32,13 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  let repository = repositories.find(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (!repository) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
-
-  repositories[repositoryIndex] = repository;
+  repository = { ...repository, ...updatedRepository, likes: repository.likes };
 
   return response.json(repository);
 });
