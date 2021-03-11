5b4045a3f93e5ad11b6c5a99f722e0747ede53b5
Eric Martins <eric.martins@radixeng.com.br>
2021-03-11 18:36:56 -0300

    fix post method to create add a new repository

--- src/index.js
+++ src/index.js
@@ -23,6 +23,8 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
+  repositories.push(repository)
+
   return response.status(201).json(repository);
 });
 
@@ -48,7 +50,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
